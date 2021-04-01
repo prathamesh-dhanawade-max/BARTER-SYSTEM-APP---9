c12810198bda328e78089fa74afc6b4939cbf90e
prathamesh-dhanawade-max <80527680+prathamesh-dhanawade-max@users.noreply.github.com>
2021-04-01 18:47:14 +0530

    Add files via upload

---
+++ SettingScreen.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  ImageBackground,
+  Platform,
+} from "react-native";
+import { DrawerItems } from "react-navigation-drawer";
+import { Avatar } from "react-native-elements";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
+import firebase from "firebase";
+import db from "../config";
+import axios from "axios";
+
+export default class SettingScreen extends Component {
+}
